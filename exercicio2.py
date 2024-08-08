1a8576c71c75bee9a1414b8cc47f7178ed8bcc03
Kauany7492 <140413644+Kauany7492@users.noreply.github.com>
2024-08-07 23:30:15 -0300

    resposta da questao 2

---
+++ exercicio2.py
@@ -0,0 +1,8 @@
+#2)Fazer um código que pergunte dois números e mostre todos os números entre esses dois.
+
+num1 = int(input("Digite um número: "))
+num2 = int(input("Digite outro número: "))
+
+while num1 < num2 :
+    num1 += 1
+    print(num1) 
