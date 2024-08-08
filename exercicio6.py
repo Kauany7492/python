5a9f24d207e1805df713deedc53120b63b09583f
Kauany7492 <140413644+Kauany7492@users.noreply.github.com>
2024-08-07 23:55:00 -0300

    resposta da questao 6

---
+++ exercicio6.py
@@ -0,0 +1,16 @@
+#6)Fazer um código que mostre a lista de contas e remova uma delas
+
+nomes = ["Alana", "Nata", "Rafhael", "Lucas", "Kauany"]
+
+def rev(a):
+    nomes.remove(a)
+
+while len(nomes) > 4:
+    print(nomes)
+    esco = input("Escolha um para remover: ")
+
+    if esco == "Alana" or esco == "Nata" or esco == "Rafhael" or esco == "Lucas" or esco == "Kauany":
+        rev(esco)
+
+    else:
+        print("Escolha inválida")
