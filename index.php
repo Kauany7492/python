d91161e316418f130e6c8287ac7924aabea853e7
Kauany7492 <140413644+Kauany7492@users.noreply.github.com>
2024-08-12 17:29:51 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,20 @@
 <h1>My first PHP page</h1>
 
 <?php
-echo "Hello World!";
-echo "php";
+class carro{
+  public $cor;
+  public $modelo/
+  public function __construct($cor, $modelo){
+    $this -> cor = $cor;
+    $this -> modelo = $modelo;
+  }
+  public function mensage(){
+    return "Meu carro é ". $this -> modelo. " ". $this -> cor."!";
+  }
+}
+
+$meucarro = new carro("azul", "stilo");
+var_dump($meucarro);
 ?>
 
 </body>
