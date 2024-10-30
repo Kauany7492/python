753bc66db2484accaf59126102c6aff408081f1f
Kauany7492 <140413644+Kauany7492@users.noreply.github.com>
2024-10-30 15:47:45 -0300

    Create Calculadora_bhaskara.py

---
+++ Projetos/Calculadora_bhaskara.py
@@ -0,0 +1,19 @@
+# calcular o valor de x com a formula de bhaskara 
+import math
+
+def bhaskara( a, b, c ):
+    print(f"\n\n{a}•x²+{b}•x+{c}=0")
+    delta = b ** 2 - 4 * a * c
+    raiz_delta = math.sqrt( b ** 2 - 4 * a * c )
+    print(f"\n ∆ = {delta}")
+    print(f"\n √∆ = {raiz_delta}")
+    x1 = ( - b + raiz_delta ) / ( 2 * a )
+    x2 = ( - b - raiz_delta ) / ( 2 * a )
+    
+    print(f"\nS = ({x1};{x2})")
+
+valor_a = int(input("\nDigite o valor de a: "))
+valor_b = int(input("\nDigite o valor de b: "))
+valor_c = int(input("\nDigite o valor de c: "))
+bhaskara(valor_a, valor_b, valor_c)
+
