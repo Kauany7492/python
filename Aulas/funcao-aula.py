8ae9683b8fdb9d0339355c7d9656ddec049eeded
Kauany7492 <140413644+Kauany7492@users.noreply.github.com>
2024-10-31 16:09:35 -0300

    Update funcao-aula.py

--- Aulas/funcao-aula.py
+++ Aulas/funcao-aula.py
@@ -1,7 +1,13 @@
+#Neste arquivo você verá o básico sobre o que é e como funciona as funções em python.
+
+
+
 #Uma função é um bloco de código que só é executado quando é chamado.
+
 #Como resultado, uma função pode retornar dados.
-#def nome-da-funcao(): 
-#  | o que vai ocorrer quando a função for chamada
+    
+    #def nome-da-funcao(): 
+    #  | o que vai ocorrer quando a função for chamada
 
 #Ex.:
 
@@ -11,8 +17,9 @@ def funcao():
 funcao()
 
 #Podemos adicionar um argumento especifico dentro dos parenteses.
-#def nome-da-funcao(argumento):
-#  | o que vai ocorrer quando a função for chamada
+   
+    #def nome-da-funcao(argumento):
+    #  | o que vai ocorrer quando a função for chamada
 
 #Ex.:
 
