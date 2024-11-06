c1b46f58f8dcb75d13e996b1ca6b17a6dbef7cef
Kauany7492 <140413644+Kauany7492@users.noreply.github.com>
2024-11-06 16:43:17 -0300

    Update else-aula.py

--- Aulas/Condicionais/else-aula.py
+++ Aulas/Condicionais/else-aula.py
@@ -1,1 +1,25 @@
+# O QUE É:
+  # verifica se uma condição é verdadeira. Caso ela seja verdadeira ele execulta o código contido dentro dele.
 
+# SINTAXE:
+  # if condição:
+  #  | código a ser execultado caso a condição seja verdadeira.
+
+# Ex.:
+a = 0
+b = 3
+
+if b > a:
+  print(f"{a} é menor que {b}.")
+
+# Ex.:
+if not b < a:
+  print(f"{a} é menor que {b}")
+
+# Ex.:
+if a == 0 and b >= 3:
+  print("a é igual a 0 e b é maior ou igual a 3")
+
+# Ex.:
+if a < b or b == 0:
+  print(f"{a}, {b}")
