477fbce2e9892aa44edec61a1868168a697db36d
Kauany7492 <140413644+Kauany7492@users.noreply.github.com>
2024-11-06 16:07:22 -0300

    Create elif-aula.py

---
+++ Aulas/Condicionais/elif-aula.py
@@ -0,0 +1,18 @@
+# O QUE É:
+  # O elif fuunciona em conjunto com o if, ele verifica se outra condição é verdadeira quando o if for falso.
+
+# SINTAXE:
+  # elif condição:
+  #  | código a ser execultado caso a condição for verdadeira
+
+# CONDICIONAIS:
+  # São as mesmas condicionais que o if.
+
+# Ex.:
+a = 0
+b = 7
+if b < a:
+  print(f"{b} é menor que {a}")
+elif b > a:
+  print(f"{b} é maior que {a}")
+
