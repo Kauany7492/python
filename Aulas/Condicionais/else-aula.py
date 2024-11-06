dc359bb491ae8fd2180a8672140d83827e78d30d
Kauany7492 <140413644+Kauany7492@users.noreply.github.com>
2024-11-06 16:48:03 -0300

    Update else-aula.py

--- Aulas/Condicionais/else-aula.py
+++ Aulas/Condicionais/else-aula.py
@@ -1,9 +1,9 @@
 # O QUE É:
-  # verifica se uma condição é verdadeira. Caso ela seja verdadeira ele execulta o código contido dentro dele.
+  # O else funciona em conjunto com o if e o elif, ele é execultado quando o if e o elif são falsos.
 
 # SINTAXE:
-  # if condição:
-  #  | código a ser execultado caso a condição seja verdadeira.
+  # else:
+  #  | código a ser execultado caso o if e o elif forem falsos.
 
 # Ex.:
 a = 0
@@ -11,15 +11,24 @@ b = 3
 
 if b > a:
   print(f"{a} é menor que {b}.")
+else:
+  print(f"{a} é maior que {b}.")
 
 # Ex.:
 if not b < a:
   print(f"{a} é menor que {b}")
+else:
+  print(f"{b} é menor que {a}")
 
 # Ex.:
 if a == 0 and b >= 3:
   print("a é igual a 0 e b é maior ou igual a 3")
+else:
+  print("a não é igual a 0 e b não é maior ou igual a 3")
 
 # Ex.:
 if a < b or b == 0:
   print(f"{a}, {b}")
+else:
+   print(f"{a}, {b}")
+  
