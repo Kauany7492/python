210e57c16f74b253ce7b1c3eef40d9e964c88dba
Kauany7492 <140413644+Kauany7492@users.noreply.github.com>
2024-11-06 15:52:25 -0300

    Create if-aula.py

---
+++ Aulas/Condicionais/if-aula.py
@@ -0,0 +1,23 @@
+# O QUE É:
+  # verifica se uma condição é verdadeira. Caso ela seja verdadeira ele execulta o código contido dentro dele.
+
+# SINTAXE:
+  # if condição:
+  #  | código a ser execultado caso a condição seja verdadeira.
+  # podemos declarar o if em uma linha apenas da seguinte maneira:
+    # if condição: código a ser execultado caso a condição seja verdadeira
+
+
+# CONDICIONAIS:
+  # < ou > → menor que ou maior que
+  # <= ou >= → menor ou igual a ou maior ou igual a
+  # == → igual
+  # != → diferente
+
+# Ex.:
+a = 0
+b = 3
+
+if b > a:
+  print(f"{a} é menor que {b}.")
+
