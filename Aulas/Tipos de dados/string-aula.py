0e920b0fbf07fa4f7a9ca3c04c9f732b2c7b4788
Kauany7492 <140413644+Kauany7492@users.noreply.github.com>
2024-11-11 15:32:56 -0300

    Create string-aula.py

---
+++ Aulas/Tipos de dados/string-aula.py
@@ -0,0 +1,24 @@
+# O QUE SÃO:
+  # são palavras ou números cercados pos aspas( '' → simples ou "" → duplas)
+  # podem ser inseridas em variáveis e em algumas funções, como "print()"
+
+#Ex.: 
+a = "oi"
+print(a)
+print("ola")
+
+# FUNÇÃO LEN():
+  # retorna o comprimento da string.
+
+#Ex.:
+b = "bom dia"
+print(len(b))
+
+# Função upper():
+  # transforma a string toda em maiúscula.
+
+#Ex.:
+c = "canada"
+print(upper(c))
+
+# Função lower():
