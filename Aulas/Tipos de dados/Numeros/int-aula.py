e848a5005e76423cc6133d8aa3d345c182203778
Kauany7492 <140413644+Kauany7492@users.noreply.github.com>
2024-11-18 15:36:36 -0300

    Create int-aula.py

---
+++ Aulas/Tipos de dados/Numeros/int-aula.py
@@ -0,0 +1,16 @@
+# O que é:
+  # um número inteiro podendo ser positivo ou negativo, mas sem decimais.
+
+# Ex.:
+a = 3
+b = -5
+
+# Função type():
+  # mostra qual é o tipo de dado dentro de uma variável.
+
+# Sintaxe:
+  # type(variável)
+
+#Ex.:
+print(type(a))
+print(type(b))
