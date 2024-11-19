59550b019556b0474f0bca3d2f09015e6a60014e
Kauany7492 <140413644+Kauany7492@users.noreply.github.com>
2024-11-19 14:16:09 -0300

    Update int-aula.py

--- Aulas/Tipos de dados/Numeros/int-aula.py
+++ Aulas/Tipos de dados/Numeros/int-aula.py
@@ -14,3 +14,27 @@ b = -5
 #Ex.:
 print(type(a))
 print(type(b))
+
+# Conversão:
+  # você pode converter números decimais em números inteiros.
+
+# Sintaxe:
+  # int(variavel)
+
+  # Terminal
+    # número - .decimais
+
+# Ex.:
+c = 10.5
+d = float(c)
+print(d)
+
+# Input:
+  # você pode utilizar a função "int()" para converter os dados do input de texto para números inteiros.
+
+# Sintaxe:
+  # variavel = int(input("pergunta"))
+
+#Ex.:
+e = int(input("Digite um número inteiro"))
+print(type(e))
