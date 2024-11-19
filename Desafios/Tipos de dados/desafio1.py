0db789ebd0ec8e232d491688966521b4cc1a5bd0
Kauany7492 <140413644+Kauany7492@users.noreply.github.com>
2024-11-19 14:37:38 -0300

    Create desafio1.py

---
+++ Desafios/Tipos de dados/desafio1.py
@@ -0,0 +1,7 @@
+# 1) Escreva um codigo que peça o nome e a idade de uma pessoa  e faça a conversão correta dos dados.
+#Exemplo de resposta:
+
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+
+print(f"{nome} tem {idade} anos")
