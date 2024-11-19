231d7fb4799be4a589236ed71b2a21fe32d979a4
Kauany7492 <140413644+Kauany7492@users.noreply.github.com>
2024-11-19 14:10:29 -0300

    Create float-aula.py

---
+++ Aulas/Tipos de dados/Numeros/float-aula.py
@@ -0,0 +1,30 @@
+# O que é:
+  # um número com deecimais.
+
+# Ex.:
+a = 3.5
+b = 5.0
+
+# Função type():
+  # mostra qual é o tipo de dado dentro de uma variável.
+
+# Sintaxe:
+  # type(variável)
+
+#Ex.:
+print(type(a))
+print(type(b))
+
+# Conversão:
+  # você pode converter números inteiros em números decimais.
+
+# Sintaxe:
+  # float(variavel)
+
+  # Terminal
+    # número.0
+
+# Ex.:
+c = 10
+d = float(c)
+print(d)
