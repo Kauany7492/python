9d03d45997be85ca5b8cdd45ae997b99435c0e33
Kauany7492 <140413644+Kauany7492@users.noreply.github.com>
2024-11-19 14:57:37 -0300

    Create desafio3.py

---
+++ Desafios/Tipos de dados/desafio3.py
@@ -0,0 +1,8 @@
+# 3) Escreva um código que peça um número e o converta em um texto impimindo seu tipo antes da conversão e depois.
+#Exemplo de resposta:
+
+numero = int(input("Digite um número: "))
+texto = str(numero)
+
+print(type(numero))
+print(type(texto))
