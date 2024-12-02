264c5b9d18686885dd0ba1785d202a41e29a25b6
Kauany7492 <140413644+Kauany7492@users.noreply.github.com>
2024-12-02 16:46:09 -0300

    Update aritméticos.py

--- Aulas/Operadores/aritméticos.py
+++ Aulas/Operadores/aritméticos.py
@@ -1,1 +1,60 @@
+# O QUE SÃO:
+  # São operadores matemáticos comuns.
 
+# OPERADORES ARITMÉTICOS:
+  # - Adição (+)
+  # - Subtração (-)
+  # - Multiplicação (*)
+  # - Divisão (/)
+  # - Resto (%)
+  # - Divisão com resultado arredondado (//)
+  # - Potenciação (**)
+
+# ADIÇÃO (+):
+  # retorna soma de dois ou mais números.
+  # Ex.: 2 + 2 retorna 4
+
+a = 2 + 2
+print(a)
+
+# SUBTRAÇÃO (-):
+  # retorna subtração de dois ou mais números.
+  # Ex.: 3 - 2 retorna 1
+
+b = 3 - 2
+print(b)
+
+# MULTIPLICAÇÃO (*):
+  # retorna multiplicação de dois ou mais números.
+  # Ex.: 3 * 5 retorna 15
+
+c = 3 * 5
+print(c)
+
+# DIVISÃO (/):
+  # rtorna divisão de um número por outro.
+  # Ex.: 10 / 5 retorna 2
+
+d = 10 / 5
+print(d)
+
+# RESTO (%):
+  # retorna o resto da divisão de um número por outro.
+  # Ex.: 5 % 2 retorna 1
+
+e = 5 % 2
+print(e)
+
+# DIVISÃO COM RESULTADO ARREDONDADO (//):
+  # retorna o resultado da divisão arredondado (retira a parte decimal).
+  # Ex.: 15 // 7 retorna 2
+
+f = 15 // 7
+print(f)
+
+# POTENCIAÇÃO (**):
+  # retorna um numero multiplicado por ele mesmo por um número determinado.
+  # Ex.: 3 ** 2 retorna 9
+
+g = 3 ** 2
+print(g)
